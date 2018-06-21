15052bbc5b48a7826b9ca5bc4d92a561dc1693a7
tarasii <tarasii@gmail.com>
2018-06-21 10:56:30 +0300

    Update svdj.py

--- svdj.py
+++ svdj.py
@@ -1,50 +1,49 @@
 '''
 SVDJ Singular value decomposition using Jacobi algorithm.
+  u, s, v = svdj(a)
 Was adapted from Nicolas Le Bihan and Stephen J. Sangwine svdj matlab function
 http://freesourcecode.net/matlabprojects/5649/sourcecode/svdj.m#.WyokgfZuKUk
-
 '''
 
 import numpy as np
 
 def svdj(a, n_it=2):
 	m, n = np.shape(a)
+	i_r_c = [[i, r, c] for i in range(n_it) for r in range(n-1) for c in range (r+1, n)]
                                             # F = str2func(class(A));
 	v = np.eye(n, dtype=type(a[0, 0]))  # V = F(eye(N));
 	
 	cnt = 0
-	for i in range(n_it):
-		for r in range(n - 1):
-			for c in range (r + 1, n):
-				a1 = a[:, r]
-				a2 = a[:, c]
+	for i, r, c in i_r_c:
+		a1 = a[:, r]
+		a2 = a[:, c]
 
-				b_rr = sum(abs(a1)**2)          # b_rr = sum(abs(A(:,r)).^2);
-				b_cc = sum(abs(a2)**2)          # b_cc = sum(abs(A(:,c)).^2);
-				b_rc = np.dot(a1.conj().T , a2) # b_rc = A(:,r)' * A(:,c);
-				m_b_rc = abs(b_rc);             # m = abs(b_rc);
+		b_rr = sum(abs(a1)**2)          # b_rr = sum(abs(A(:,r)).^2);
+		b_cc = sum(abs(a2)**2)          # b_cc = sum(abs(A(:,c)).^2);
+		b_rc = np.dot(a1.conj().T , a2) # b_rc = A(:,r)' * A(:,c);
+		m_b_rc = abs(b_rc);             # m = abs(b_rc);
 
-				if not m_b_rc:
-					continue
+		if not m_b_rc:
+			continue
 
-				tau_m = (b_cc - b_rr) / (2 * m_b_rc)   # tau = (b_cc - b_rr) / (2 * m);
+		tau_m = (b_cc - b_rr) / (2 * m_b_rc)   # tau = (b_cc - b_rr) / (2 * m);
 
-				if not tau_m:
-					continue
+		if not tau_m:
+			continue
 
-				cnt += 1
-				t = np.sign(tau_m) / (abs(tau_m) + np.sqrt(1.0 + tau_m**2))  # t = sign(tau) ./ (abs(tau) + sqrt(1 + tau .^ 2));
-				c_t = 1 / np.sqrt(1.0 + t**2)                                # C = 1 ./ sqrt(1 + t .^ 2); 
-				s = b_rc * t * c_t / m_b_rc                                  # S = (b_rc .* t .* C) ./ m;
+		cnt += 1
+		t = np.sign(tau_m) / (abs(tau_m) + np.sqrt(1.0 + tau_m**2))  # t = sign(tau) ./ (abs(tau) + sqrt(1 + tau .^ 2));
+		c_t = 1 / np.sqrt(1.0 + t**2)                                # C = 1 ./ sqrt(1 + t .^ 2); 
+		s = b_rc * t * c_t / m_b_rc                                  # S = (b_rc .* t .* C) ./ m;
 
-				g = np.eye(n, dtype=type(a[0, 0])) # G = F(eye(N));
-				g[r, r] = c_t                      # G(r,r) = F(C);
-				g[c, c] = c_t                      # G(c,c) = F(C);
-				g[r, c] = s                        # G(r,c) = S;
-				g[c, r] = -np.conj(s)              # G(c,r) =-conj(S);
+		g = np.eye(n, dtype=type(a[0, 0])) # G = F(eye(N));
+		g[r, r] = c_t                      # G(r,r) = F(C);
+		g[c, c] = c_t                      # G(c,c) = F(C);
+		g[r, c] = s                        # G(r,c) = S;
+		g[c, r] = -np.conj(s)              # G(c,r) =-conj(S);
 
-				a = np.dot(a, g)                   # A = A * G;
-				v = np.dot(v, g)                   # V = V * G;
+		a = np.dot(a, g)                   # A = A * G;
+		v = np.dot(v, g)                   # V = V * G;
 
 
 	if cnt == 0:
