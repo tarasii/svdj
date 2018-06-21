c49a7d0b09a4deacb675d2421836937951304cc3
tarasii <tarasii@gmail.com>
2018-06-21 13:05:12 +0300

    Update svdj.py
    tolerance tested on data

--- svdj.py
+++ svdj.py
@@ -7,48 +7,58 @@ http://freesourcecode.net/matlabprojects/5649/sourcecode/svdj.m#.WyokgfZuKUk
 
 import numpy as np
 
-def svdj(a, n_it=2):
+def svdj(a, n_it=4):
+	#tol = 2.2204e-016
+	tol = 1.0e-014
+
 	m, n = np.shape(a)
-	i_r_c = [[i, r, c] for i in range(n_it) for r in range(n-1) for c in range (r+1, n)]
+	r_c = [[r, c] for r in range(n-1) for c in range (r+1, n)]
                                             # F = str2func(class(A));
 	v = np.eye(n, dtype=type(a[0, 0]))  # V = F(eye(N));
 	
-	cnt = 0
-	for i, r, c in i_r_c:
-		a1 = a[:, r]
-		a2 = a[:, c]
+	on = sum(sum(abs(a)**2))/n  # On = 0; for c = A, On = On + sum(abs(c).^2); end; On = On ./ N;
 
-		b_rr = sum(abs(a1)**2)          # b_rr = sum(abs(A(:,r)).^2);
-		b_cc = sum(abs(a2)**2)          # b_cc = sum(abs(A(:,c)).^2);
-		b_rc = np.dot(a1.conj().T , a2) # b_rc = A(:,r)' * A(:,c);
-		m_b_rc = abs(b_rc);             # m = abs(b_rc);
+	for i in range(n_it):
+		cnt = 0
+		for r, c in r_c:
+			a1 = a[:, r]
+			a2 = a[:, c]
 
-		if not m_b_rc:
-			continue
+			b_rr = sum(abs(a1)**2)          # b_rr = sum(abs(A(:,r)).^2);
+			b_cc = sum(abs(a2)**2)          # b_cc = sum(abs(A(:,c)).^2);
+			b_rc = np.dot(a1.conj().T , a2) # b_rc = A(:,r)' * A(:,c);
+			m_b_rc = abs(b_rc);             # m = abs(b_rc);
 
-		tau_m = (b_cc - b_rr) / (2 * m_b_rc)   # tau = (b_cc - b_rr) / (2 * m);
+			if not m_b_rc:
+				continue
 
-		if not tau_m:
-			continue
+			tau_m = (b_cc - b_rr) / (2 * m_b_rc)   # tau = (b_cc - b_rr) / (2 * m);
 
-		cnt += 1
-		t = np.sign(tau_m) / (abs(tau_m) + np.sqrt(1.0 + tau_m**2))  # t = sign(tau) ./ (abs(tau) + sqrt(1 + tau .^ 2));
-		c_t = 1 / np.sqrt(1.0 + t**2)                                # C = 1 ./ sqrt(1 + t .^ 2); 
-		s = b_rc * t * c_t / m_b_rc                                  # S = (b_rc .* t .* C) ./ m;
+			if not tau_m:
+				continue
 
-		g = np.eye(n, dtype=type(a[0, 0])) # G = F(eye(N));
-		g[r, r] = c_t                      # G(r,r) = F(C);
-		g[c, c] = c_t                      # G(c,c) = F(C);
-		g[r, c] = s                        # G(r,c) = S;
-		g[c, r] = -np.conj(s)              # G(c,r) =-conj(S);
+			cnt += 1
+			t = np.sign(tau_m) / (abs(tau_m) + np.sqrt(1.0 + tau_m**2))  # t = sign(tau) ./ (abs(tau) + sqrt(1 + tau .^ 2));
+			c_t = 1 / np.sqrt(1.0 + t**2)                           # C = 1 ./ sqrt(1 + t .^ 2); 
+			s = b_rc * t * c_t / m_b_rc                             # S = (b_rc .* t .* C) ./ m;
+			
+			g = np.eye(n, dtype=type(a[0, 0]))                     # G = F(eye(N));
+			g[r, r] = c_t                                          # G(r,r) = F(C);
+			g[c, c] = c_t                                          # G(c,c) = F(C);
+			g[r, c] = s                                            # G(r,c) = S;
+			g[c, r] = -np.conj(s)                                  # G(c,r) =-conj(S);
 
-		a = np.dot(a, g)                   # A = A * G;
-		v = np.dot(v, g)                   # V = V * G;
+			a = np.dot(a, g)                                       # A = A * G;
+			v = np.dot(v, g)                                       # V = V * G;
 
+		if cnt == 0:
+			raise Exception('No rotations performed during sweep.')
 
-	if cnt == 0:
-		raise Exception('No rotations performed during sweep.')
+		b = np.dot(a.conj().T, a)               # B = A' * A;
+		off = sum(sum(abs(np.triu(b, 1))**2))/n	# Off = sum(sum(abs(triu(B, 1))))/(N.^2);
 
+		if off/on < tol:
+			break
 
 	b = np.dot(a.conj().T, a)      # B = A' * A;
 	t_ = np.sqrt(abs(np.diag(b)))  # T = sqrt(abs(diag(B)));
